f200cc1ba0e8ddb57d2316efc0bf29c7e5dc3596
yasin <yasinmiapolash@gmail.com>
2023-10-06 23:20:48 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,224 +4,121 @@ void main() {
   runApp(MyApp());
 }
 
-class Product {
-  final String name;
-  final String imageUrl;
-  final String color;
-  final String size;
-  double price;
-  int quantity;
-
-  Product({
-    required this.name,
-    required this.imageUrl,
-    required this.color,
-    required this.size,
-    this.price = 0.0,
-    this.quantity = 1,
-  });
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ShoppingCartScreen(),
+      home: MyButtonScreen(),
     );
   }
 }
 
-class ShoppingCartScreen extends StatefulWidget {
+class MyButtonScreen extends StatefulWidget {
   @override
-  _ShoppingCartScreenState createState() => _ShoppingCartScreenState();
+  _MyButtonScreenState createState() => _MyButtonScreenState();
 }
 
-class _ShoppingCartScreenState extends State<ShoppingCartScreen> {
-  List<Product> products = [
-    Product(
-      name: 'Dress 1',
-      imageUrl: 'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      color: 'Red',
-      size:'L' ,
-      price: 50.0,
-    ),
-    Product(
-      name: 'Dress 2',
-      imageUrl:
-                'https://images.pexels.com/photos/3907507/pexels-photo-3907507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+class _MyButtonScreenState extends State<MyButtonScreen> {
+  String selectedSize = '';
+  late Color selectedColor;
 
-        color: 'Blue',
-      size: 'M',
-      price: 60.0,
-    ),
-    Product(
-      name: 'Dress 3',
-      imageUrl:
-          'https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      color: 'Green',
-      size: 'M',
-      price: 45.0,
-    ),
-  ];
-
-  double getTotalPrice() {
-    double totalPrice = 0.0;
-    for (var product in products) {
-      totalPrice += product.price * product.quantity;
-    }
-    return totalPrice;
-  }
-  void _showCongratulatorySnackbar(BuildContext context) {
+  void _showSnackbar(String size) {
     final snackBar = SnackBar(
-      content: Text('Congratulations! Your order has been checked out.'),
-      duration: Duration(seconds: 3), // Adjust the duration as needed
+      content: Text('Selected Size: $size'),
+      duration: Duration(seconds: 3),
     );
-
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
+
+  void _onButtonTap(String size, Color color) {
+    setState(() {
+      selectedSize = size;
+      selectedColor = color;
+    });
+    _showSnackbar(size);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Size Selector'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: Row(
 
-      body: Padding(
-        padding: const EdgeInsets.only(top: 60),
-        child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            final screenWidth = constraints.maxWidth;
-            final screenHeight = constraints.maxHeight;
-
-            final isSmallScreen = screenWidth < 600;
-
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Icon(Icons.search_rounded,size: 40,),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'My Bag',
-                    style: TextStyle(fontSize: isSmallScreen ? 20.0 : 24.0, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: products.length,
-                    itemBuilder: (context, index) {
-                      final product = products[index];
-                      return ListTile(
-                        leading: Image.network(
-                          product.imageUrl, // Use the image URL from your data
-                          width: isSmallScreen ? 100.0 : 120.0,
-                          height: isSmallScreen ? 100.0 : 120.0,
-                         // fit: BoxFit.,
-                        ),
-                        title: Padding(
-                          padding: const EdgeInsets.only(top: 15),
-                          child: Text(
-                            product.name,
-                            style: TextStyle(fontSize: isSmallScreen ? 16.0 : 20.0),
-                          ),
-                        ),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-
-                              children: [
-                                Text('Color: ${product.color}'),
-                                SizedBox(width: 10,),
-
-                                Text('Size: ${product.size}'),
-                              ],
-                            ),
-                            Row(
-                              children: <Widget>[
-                                IconButton(
-                                  icon: Icon(Icons.remove),
-                                  onPressed: () {
-                                    setState(() {
-                                      if (product.quantity > 0) {
-                                        product.quantity--;
-                                      }
-                                    });
-                                  },
-                                ),
-                                Text('${product.quantity}'),
-                                IconButton(
-                                  icon: Icon(Icons.add),
-                                  onPressed: () {
-                                    setState(() {
-                                      product.quantity++;
-                                    });
-                                  },
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        trailing: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(Icons.menu_sharp),
-                            Text(
-                              '\$${(product.price * product.quantity).toStringAsFixed(2)}',
-                              style: TextStyle(fontSize: isSmallScreen ? 16.0 : 20.0),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(bottom: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Total amount:',
-                        style: TextStyle(fontSize: isSmallScreen ? 16.0 : 20.0, fontWeight: FontWeight.bold),
+                 // mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () => _onButtonTap('S', Colors.green),
+                      style: ElevatedButton.styleFrom(
+                        primary: selectedSize == 'S' ? Colors.orange : Colors.grey,
                       ),
-                    Text(
-                    '\$${getTotalPrice().toStringAsFixed(2)}',
-                      style: TextStyle(fontSize: isSmallScreen ? 16.0 : 20.0, fontWeight: FontWeight.bold),
+                      child: Text('S'),
+                    ),
+                    SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () => _onButtonTap('M', Colors.blue),
+                      style: ElevatedButton.styleFrom(
+                        primary: selectedSize == 'M' ? Colors.orange : Colors.grey,
+                      ),
+                      child: Text('M'),
+                    ),
+                    SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () => _onButtonTap('L', Colors.red),
+                      style: ElevatedButton.styleFrom(
+                        primary: selectedSize == 'L' ? Colors.orange : Colors.grey,
+                      ),
+                      child: Text('L'),
+                    ),
+                    SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () => _onButtonTap('XL', Colors.red),
+                      style: ElevatedButton.styleFrom(
+                        primary: selectedSize == 'XL' ? Colors.orange : Colors.grey,
+                      ),
+                      child: Text('Xl'),
                     ),
 
-
-
-
-                    ],
-                  ),
+                  ],
                 ),
-                Container(
-
-                  decoration: BoxDecoration(
-                    color: Colors.redAccent,
-                    borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(children: [
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => _onButtonTap('XXL', Colors.red),
+                    style: ElevatedButton.styleFrom(
+                      primary: selectedSize == 'XXL' ? Colors.orange : Colors.grey,
+                    ),
+                    child: Text('XXl'),
                   ),
-                  padding: EdgeInsets.only(bottom: 10),
-                  width: 400,
-                  height: 40,
-                  child:TextButton(
-                    onPressed: (){
-                      _showCongratulatorySnackbar(context);
-
-
-                    },
-                    child:Text('Checkout', style: TextStyle(fontSize: isSmallScreen ? 16.0 : 20.0)),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => _onButtonTap('XXXL', Colors.red),
+                    style: ElevatedButton.styleFrom(
+                      primary: selectedSize == 'XXXL' ? Colors.orange : Colors.grey,
+                    ),
+                    child: Text('XXXL'),
                   ),
-                ),
-              ],
-            );
-          },
+                ],),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
 }
+
